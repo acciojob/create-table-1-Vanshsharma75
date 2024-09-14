5c256072f4da1b41264c2e36f9b44bc250c9b180
vansh sharma <112168620+Vanshsharma75@users.noreply.github.com>
2024-09-14 12:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+
 function insert_Row() {
     //Write your code here
-  
-  
+let  table=document.querryselector("#smapleTable");
+let  newrow=document.insertRow(0);
+let cell1=newrow.insertCell();
+let cell2=newrow.insertCell();
+cell1.innertext="new cell"
+cell2.innertext="new cell2"
 }
